a69324404f1a1e9d12b919e63de1ddfb7d15ffb4
yuekunyu <yuekunyu@tencent.com>
2026-08-29 11:45:59 +0800

    [bugfix][#7] preserve dedup in residual semi join replacement

--- test/dsl/e2e/sql/left_semijoin_under_gb_replacement.sql
+++ test/dsl/e2e/sql/left_semijoin_under_gb_replacement.sql
@@ -1,8 +1,8 @@
-SELECT c.id, c.parent_id
-FROM dsl_fk_child AS c
+SELECT p.id
+FROM dsl_fk_parent AS p
 WHERE EXISTS (
   SELECT 1
-  FROM dsl_fk_parent AS p
+  FROM dsl_fk_child AS c
   WHERE c.parent_id = p.id
     AND c.id + p.id > 0)
-ORDER BY c.id, c.parent_id;
+ORDER BY p.id;
