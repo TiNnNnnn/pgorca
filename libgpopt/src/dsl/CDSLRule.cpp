9d9464d00e540654f620680f2d521d67193de615
yuekunyu <yuekunyu@tencent.com>
2026-08-29 10:28:46 +0800

    [feature][#7] support join predicate DSL bindings

--- libgpopt/src/dsl/CDSLRule.cpp
+++ libgpopt/src/dsl/CDSLRule.cpp
@@ -82,7 +82,8 @@ CDSLOp::CDSLOp(CMemoryPool *,  // mp unused: children/syms arrays pre-built
 	GPOS_ASSERT(CDSLOpKindTable::UlSyms(edslop) == pdrgpsym->Size() ||
 				(EdslopAgg == edslop && 5 == pdrgpsym->Size()) ||
 				((EdslopInnerJoin == edslop || EdslopLeftJoin == edslop) &&
-				 2 == pdrgpsym->Size()) ||
+				 (2 == pdrgpsym->Size() || 4 == pdrgpsym->Size() ||
+				  5 == pdrgpsym->Size())) ||
 				(EdslopUnion == edslop && 0 == pdrgpsym->Size()));
 }
 
