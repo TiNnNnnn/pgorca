ea7ec1d5186e1a1c34a56540702636bc65eb5e1f
yuekunyu <yuekunyu@tencent.com>
2026-08-30 22:21:23 +0800

    [feature][#7] lower boolean value subqueries

---
+++ test/dsl/e2e/sql/select2apply_exists_disjunction_identity_replacement.sql
@@ -0,0 +1,9 @@
+SELECT o.id
+FROM dsl_insub_outer AS o
+WHERE o.id = 3
+   OR EXISTS (
+        SELECT 1
+        FROM dsl_correlated_exists AS i
+        WHERE i.k = o.id
+          AND i.payload > 20)
+ORDER BY o.id;
