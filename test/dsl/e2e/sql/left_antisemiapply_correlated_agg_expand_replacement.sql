c3784a1b9c8c74377c1b6668022bea02d310c01c
yuekunyu <yuekunyu@tencent.com>
2026-08-30 13:16:54 +0800

    [feature][#7] support anti apply aggregate pullup

---
+++ test/dsl/e2e/sql/left_antisemiapply_correlated_agg_expand_replacement.sql
@@ -0,0 +1,9 @@
+SELECT o.g
+FROM dsl_agg_outer AS o
+WHERE NOT EXISTS (
+    SELECT count(*)
+    FROM dsl_agg_outer AS i
+    WHERE i.g = o.g
+    GROUP BY i.v
+    HAVING i.v = i.v)
+ORDER BY o.g;
