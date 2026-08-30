9d5bc9686089ad47ca75464c0c3ed193631619ff
yuekunyu <yuekunyu@tencent.com>
2026-08-30 23:56:57 +0800

    [test][#7] tighten cascaded DSL assertions

--- test/dsl/e2e/sql/tableeq_negative.sql
+++ test/dsl/e2e/sql/tableeq_negative.sql
@@ -1,6 +1,5 @@
 SELECT id, v
 FROM dsl_insub_outer o
 WHERE id IN (SELECT id FROM dsl_insub_inner i1)
-  AND id IN (SELECT id FROM dsl_insub_outer i2)
+  AND id IN (SELECT id FROM dsl_notin_tag i2)
 ORDER BY id;
-
