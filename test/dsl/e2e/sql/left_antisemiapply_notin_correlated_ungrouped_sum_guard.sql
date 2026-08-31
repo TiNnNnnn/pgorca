a849676b7db849c1f13fcaac175db694c38a27b8
yuekunyu <yuekunyu@tencent.com>
2026-08-31 14:51:32 +0800

    [fix][#7] recognize safe integer aggregates

---
+++ test/dsl/e2e/sql/left_antisemiapply_notin_correlated_ungrouped_sum_guard.sql
@@ -0,0 +1,8 @@
+SELECT o.case_id
+FROM dsl_notin_outer AS o
+WHERE o.x NOT IN (
+    SELECT sum(i.y)
+    FROM dsl_notin_inner AS i
+    WHERE i.set_id = o.set_id
+    GROUP BY i.y)
+ORDER BY o.case_id;
