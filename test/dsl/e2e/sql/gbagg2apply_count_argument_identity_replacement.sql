10aa187f5a7ca5b90538ee4f7df50da6f0deab40
yuekunyu <yuekunyu@tencent.com>
2026-09-02 16:52:13 +0800

    [test][#7] verify aggregate count direct lowering

--- test/dsl/e2e/sql/gbagg2apply_count_argument_identity_replacement.sql
+++ test/dsl/e2e/sql/gbagg2apply_count_argument_identity_replacement.sql
@@ -1,5 +1,5 @@
 SELECT sum((
          SELECT count(*)
          FROM dsl_correlated_exists AS i
-         WHERE i.k = o.id)) AS matched_rows
+         WHERE i.k = o.id) + 1) AS compensated_rows
 FROM dsl_insub_outer AS o;
