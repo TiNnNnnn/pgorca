9cc15ebc65211b579b7bca54672b04f14275fd69
yuekunyu <yuekunyu@tencent.com>
2026-09-04 15:00:58 +0800

    [test][#7] validate scalar join subquery routing

---
+++ test/dsl/e2e/sql/subq_join2apply_scalar_left_replacement.sql
@@ -0,0 +1,9 @@
+SELECT l.k, r.k
+FROM dsl_eq_left AS l
+JOIN dsl_eq_right AS r
+  ON l.k = r.k
+ AND l.k = (
+       SELECT max(i.payload)
+       FROM dsl_correlated_exists AS i
+       WHERE i.k = l.k)
+ORDER BY l.k, r.k;
