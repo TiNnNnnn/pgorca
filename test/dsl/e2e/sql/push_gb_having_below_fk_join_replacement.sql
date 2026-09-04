839b66b3ecce61899c2ef027508b8bbb29d58ce6
yuekunyu <yuekunyu@tencent.com>
2026-09-04 21:28:37 +0800

    [feature][#7] push aggregates below fk joins

---
+++ test/dsl/e2e/sql/push_gb_having_below_fk_join_replacement.sql
@@ -0,0 +1,6 @@
+SELECT c.parent_id, count(c.id) AS total
+FROM dsl_fk_child AS c
+INNER JOIN dsl_fk_parent AS p ON c.parent_id = p.id
+GROUP BY c.parent_id
+HAVING count(c.id) > 1
+ORDER BY c.parent_id;
