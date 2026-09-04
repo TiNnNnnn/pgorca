aed66c91cd698ec2a0c5a1d9aad722294c3e529a
yuekunyu <yuekunyu@tencent.com>
2026-09-04 20:22:22 +0800

    [feature][#7] generalize subquery sequence chaining

--- test/dsl/e2e/sql/project2apply_multiple_value_identity_replacement.sql
+++ test/dsl/e2e/sql/project2apply_multiple_value_identity_replacement.sql
@@ -4,6 +4,18 @@ SELECT o.id,
        EXISTS (
          SELECT 1
          FROM dsl_correlated_exists AS i
-         WHERE i.k = o.id) AS has_match
+         WHERE i.k = o.id) AS has_match,
+       NOT EXISTS (
+         SELECT 1
+         FROM dsl_correlated_exists AS i
+         WHERE i.k = o.id) AS is_missing,
+       o.v < ANY (
+         SELECT i.payload
+         FROM dsl_correlated_exists AS i
+         WHERE i.k = o.id) AS any_larger,
+       o.v < ALL (
+         SELECT i.payload
+         FROM dsl_correlated_exists AS i
+         WHERE i.k = o.id) AS all_larger
 FROM dsl_insub_outer AS o
 ORDER BY o.id;
