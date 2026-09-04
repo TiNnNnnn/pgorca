1e82a68e8ddf5b6a23f42ed698aecc5defd95419
yuekunyu <yuekunyu@tencent.com>
2026-09-04 21:55:10 +0800

    [test][#7] cover residual aggregate having pushdown

---
+++ test/dsl/e2e/sql/push_gb_having_residual_replacement.sql
@@ -0,0 +1,7 @@
+SELECT c.parent_id, c.id, count(c.id) AS total
+FROM dsl_fk_child AS c
+INNER JOIN dsl_fk_parent AS p
+  ON c.parent_id = p.id AND c.id < p.payload
+GROUP BY c.parent_id, c.id
+HAVING count(c.id) > 0
+ORDER BY c.id;
