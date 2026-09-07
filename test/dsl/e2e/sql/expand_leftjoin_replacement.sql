6cd59bdee0c665a10ccc6015c789cc9ba1931610
TiNnNnnn <2015810995@qq.com>
2026-09-07 23:15:40 +0800

    [bugfix][#7] preserve DSL alternative output columns

--- test/dsl/e2e/sql/expand_leftjoin_replacement.sql
+++ test/dsl/e2e/sql/expand_leftjoin_replacement.sql
@@ -1,4 +1,5 @@
-SELECT l.k AS left_k, r.k AS right_k
+SELECT l.k AS left_k, r.k AS right_k, s.k AS second_k
 FROM dsl_loj_outer AS l
 LEFT JOIN dsl_loj_inner AS r ON l.k = r.k
-ORDER BY left_k, right_k NULLS LAST;
+LEFT JOIN dsl_loj_inner AS s ON l.k = s.k
+ORDER BY left_k, right_k NULLS LAST, second_k NULLS LAST;
